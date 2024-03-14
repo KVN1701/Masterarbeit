69c6b324895b8dbbcef5ff41acb72b0eb6f407d0
Kevin <kevinell74@gmail.com>
2024-03-14 10:28:37 +0100

    first tests on accuracy

--- SimCLR.py
+++ SimCLR.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.utils.data import Subset
 import torchvision
 from sklearn.model_selection import train_test_split
+from torchmetrics import Accuracy
 
 from lightly.data import LightlyDataset
 from lightly.loss import NTXentLoss
@@ -25,11 +26,25 @@ class SimCLR(pl.LightningModule):
         return z
 
     def training_step(self, batch, batch_index):
-        (x0, x1) = batch[0]
+        x0, x1 = batch[0]
         z0 = self.forward(x0)
         z1 = self.forward(x1)
         loss = self.criterion(z0, z1)
+        accuracy = Accuracy()
+        acc = accuracy(z1, x1)
         self.log('val_loss', loss)
+        self.log('accuracy', acc, on_epoch=True)
+        return loss
+    
+    def validation_step(self, batch, batch_index):
+        x0, x1 = batch[0]
+        z0 = self.forward(x0)
+        z1 = self.forward(x1)
+        loss = self.criterion(z0, z1)
+        acccuracy = Accuracy()
+        acc = acccuracy(z1, x1)
+        self.log('val_loss', loss)
+        self.log('accuracy', acc, on_epoch=True)
         return loss
     
     def test_step(self, batch, batch_index):
@@ -37,7 +52,10 @@ class SimCLR(pl.LightningModule):
         z0 = self.forward(x0)
         z1 = self.forward(x1)
         loss = self.criterion(z0, z1)
-        self.log('test_loss', loss)
+        acccuracy = Accuracy()
+        acc = acccuracy(z1, x1)
+        self.log('val_loss', loss)
+        self.log('accuracy', acc, on_epoch=True)
         return loss
 
     def configure_optimizers(self):
@@ -53,7 +71,7 @@ def split_dataset(dataset, val_split=0.2):
     return datasets
 
 
-torch.set_float32_matmul_precision('high') # alternativ medium, da 4070ti tensor cores hat. Macht training schneller aber weniger genau
+# torch.set_float32_matmul_precision('high') # alternativ medium, da 4070ti tensor cores hat. Macht training schneller aber weniger genau
 
 model = SimCLR()
 
@@ -65,7 +83,7 @@ datasets = split_dataset(dataset)
 
 dataloader_train = torch.utils.data.DataLoader(
     datasets['train'],
-    batch_size=64,
+    batch_size=16,
     shuffle=True,
     drop_last=True,
     num_workers=8,
@@ -74,10 +92,11 @@ dataloader_train = torch.utils.data.DataLoader(
 
 dataloader_validate = torch.utils.data.DataLoader(
     datasets['val'],
-    num_workers = 23
+    num_workers = 23,
+    persistent_workers=True
 )
 
 if __name__ == '__main__':
     trainer = pl.Trainer(log_every_n_steps=2, max_epochs=10, devices=1, accelerator='gpu')
-    trainer.fit(model=model, train_dataloaders=dataloader_train)
-    trainer.test(model=model, dataloaders=dataloader_validate, ckpt_path='best')
+    trainer.fit(model=model, train_dataloaders=dataloader_train, val_dataloaders=dataloader_validate)
+    trainer.test(model=model, dataloaders=dataloader_validate, ckpt_path='last')
